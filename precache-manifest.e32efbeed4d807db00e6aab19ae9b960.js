a9128a352a74415f6db6000e796f6a1ee000d877
Amin Akhtar <aminakhtar5595@gmail.com>
2019-11-30 11:31:39 +0500

    Updates

---
+++ precache-manifest.e32efbeed4d807db00e6aab19ae9b960.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0c4f326e2667335bc53ce6e9ba4ca37",
+    "url": "/React-Traffic-Signal/index.html"
+  },
+  {
+    "revision": "a89772229321533d1de9",
+    "url": "/React-Traffic-Signal/static/css/main.f5117831.chunk.css"
+  },
+  {
+    "revision": "0739d21570b471cffc68",
+    "url": "/React-Traffic-Signal/static/js/2.a255ff87.chunk.js"
+  },
+  {
+    "revision": "a89772229321533d1de9",
+    "url": "/React-Traffic-Signal/static/js/main.8d3f1654.chunk.js"
+  },
+  {
+    "revision": "96f359a181e5a6c099e2",
+    "url": "/React-Traffic-Signal/static/js/runtime-main.b0029276.js"
+  }
+]);
